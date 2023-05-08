79b5a1d9fdcc2e040befd518b0b9e80f9c247122
Creedmastr <thegaminglife09@gmail.com>
2023-05-08 18:24:01 +0200

    simplified things

--- src/makefile/build.rs
+++ src/makefile/build.rs
@@ -2,7 +2,10 @@ use std::{fs, process::Command};
 
 pub fn build_file(file_content: String, is_rust: bool) {
     let mut final_file = String::new();
+    // Headers
+    let _ = final_file.push_str("#![allow(dead_code)]\n#![allow(non_snake_case)]\n#![allow(non_ascii_idents)]\n#![allow(unused_variables)]\n \n");
 
+    // Main content
     let _ = final_file.push_str("fn main() {\n");
     let _ = final_file.push_str(&file_content);
     let _ = final_file.push_str("\n}");
