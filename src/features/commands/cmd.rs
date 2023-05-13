f387bd4f7265403a3308d3ba4c46ed622afd18c2
Creedmastr <thegaminglife09@gmail.com>
2023-05-13 13:16:27 +0200

    fixed commands

--- src/features/commands/cmd.rs
+++ src/features/commands/cmd.rs
@@ -3,47 +3,31 @@ use crate::features::push_stringed_vector::push_stringed_vector;
 pub fn cmd_run_command_content(line: String) -> String {
     let args_stringed = line.replace("cmd:run_command(", "").replace(")", "");
 
-    let mut result: String = match &args_stringed {
-        x if x.split_whitespace().count() >= 2 => {
-            r#"        #![feature(let_chains)]
-            let process: String = match std::env::consts::FAMILY {
-        "unix" => "bash".to_string(),
-
-        "windows" => "cmd".to_string(),
-
-        &_ => todo!()
-    };
-
-    match std::env::consts::FAMILY {
-
-    "#
-            .to_string()
-        }
-
-        _ => r#"        let process: String = match std::env::consts::FAMILY {
+    let mut result: String = r#"        let process: String = match std::env::consts::FAMILY {
             "unix" => "bash".to_string(),
     
             "windows" => "cmd".to_string(),
 
             &_ => todo!()
         };
+
+        let mut output: std::process::Output;
     
         match std::env::consts::FAMILY { "#
-            .to_string(),
-    };
+        .to_string();
 
     let part2 = r#").output().expect("Couldn't execute command");"#;
 
     match args_stringed.split_whitespace().count() {
         x if x >= 2 => {
             result.push_str(
-                r#""unix" => {let _ = std::process::Command::new(process).arg("-c").args("#,
+                r#""unix" => {output = std::process::Command::new(process).arg("-c").args("#,
             );
             result.push_str(push_stringed_vector(&args_stringed, " ").as_str());
             result.push_str(r#"}"#);
             result.push_str(&part2);
             result.push_str(
-                r#""windows" => {let _ = std::process::Command::new(process).arg("/C").args("#,
+                r#""windows" => {output = std::process::Command::new(process).arg("/C").args("#,
             );
             result.push_str(push_stringed_vector(&args_stringed, " ").as_str());
             result.push_str(r#"}"#);
@@ -52,7 +36,7 @@ pub fn cmd_run_command_content(line: String) -> String {
 
         _ => {
             result.push_str(
-                r#"            "unix" => {let _ = std::process::Command::new(process).arg("-c").arg(
+                r#"            "unix" => {output = std::process::Command::new(process).arg("-c").arg(
                 "#,
             );
             result.push_str(&args_stringed);
@@ -60,18 +44,18 @@ pub fn cmd_run_command_content(line: String) -> String {
             result.push_str(r#"}"#);
             result.push_str(
                 r#"    
-            "windows" => {let _ = std::process::Command::new(process).arg("/C").arg("#,
+            "windows" => {output = std::process::Command::new(process).arg("/C").arg("#,
             );
             result.push_str(&args_stringed);
             result.push_str(&part2.replace(",", ""));
-
         }
     };
 
     result.push_str(r#"}"#);
     result.push_str("&_ => todo!()");
-    result.push_str(r#"}"#);
-
+    result.push_str(r#"};"#);
+    result.push_str(r#"println!("{}", String::from_utf8_lossy(&output.stdout));"#);
+    result.push_str(r#"println!("{}", String::from_utf8_lossy(&output.stderr));"#);
 
     return result;
 }
