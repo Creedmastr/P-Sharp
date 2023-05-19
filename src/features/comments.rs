49f7336e6d0425fbddb2f40976f62443325fdf49
Creedmastr <thegaminglife09@gmail.com>
2023-05-19 15:20:00 +0200

    removed useless todos

--- src/features/comments.rs
+++ src/features/comments.rs
@@ -1,7 +1,7 @@
 pub fn remove_comment(line: &String) -> String {
     if line.contains("# ") {
         let result: Vec<&str> = line.split("# ").collect();
-        // TODO: Only take the last one as a comment
+        
         return result[0].to_string();
         
     } else {
